50bca4ced9e31b559e5a72455d13d5d9703ef098
Lim Xuan <reconlx.official@gmail.com>
2025-10-16 02:44:56 +0800

    launch chat applications
    launches
    - telegram
    - whatsapp
    - discord

---
+++ scripts/.config/scripts/launch_chats.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+omarchy-launch-or-focus-webapp "Telegram" "https://web.telegram.org/k/" &
+omarchy-launch-or-focus-webapp "WhatsApp" "https://web.whatsapp.com/" &
+omarchy-launch-or-focus-webapp "Discord" "https://discord.com/app" &
+
+wait
